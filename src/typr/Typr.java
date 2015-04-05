9f2bd550d2be77095aa426a2dde82684bccee88a
Judy Chen <time.fly.arrow@gmail.com>
2015-04-05 02:46:36 -0400

    scan for user input

--- src/typr/Typr.java
+++ src/typr/Typr.java
@@ -1,5 +1,13 @@
 package typr;
 
-public class Typr {
+import java.util.Scanner;
 
+public class Typr {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String text;
+		
+		text = sc.nextLine();
+		System.out.println(text);
+	}
 }
