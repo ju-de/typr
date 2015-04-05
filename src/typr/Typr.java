2338597e3417412231d559df13753d5403034bd3
Judy Chen <time.fly.arrow@gmail.com>
2015-04-05 03:22:26 -0400

    add padding

--- src/typr/Typr.java
+++ src/typr/Typr.java
@@ -3,11 +3,19 @@ package typr;
 import java.util.Scanner;
 
 public class Typr {
+	public static void pad(){
+		for(int i = 0; i<10; i++)
+			System.out.println();
+	}
+	
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		String text;
-		
 		text = sc.nextLine();
-		System.out.println(text);
+		
+		do{
+			pad();
+			System.out.print(text);
+		}while(!text.equals(""));
 	}
 }
